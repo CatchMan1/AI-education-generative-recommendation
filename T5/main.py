18deef5929ad2e3452b96445476851529c1e9c03
liziqi <16322487+yirenyoulu@user.noreply.gitee.com>
2026-02-12 17:04:36 +0800

    update-revise

--- T5/main.py
+++ T5/main.py
@@ -2,29 +2,30 @@ from train import train
 from evaluate import infer
 
 params = {
-        'rec_path' : '../test_data/user_item_interact.h5',
-        'course_path': '../test_data/course_info.h5',
-        'course_id_map_path' : '../test_data/course_id_map.h5',
-        'batch_size':20,
-        'infer_size':16,
-        'num_epochs':4,
+        'rec_path' : '../data/user_item_interact.h5',
+        'course_path': '../data/course_info.h5',
+        'course_id_map_path' : '../data/course_id_map.h5',
+        'batch_size':256,
+        'infer_size':256,
+        'num_epochs':30,
         'lr':1e-4,
         'device':'cuda',
         'num_layers':2, # encoder 层数
-        'd_model':16, # encoder 隐藏层状态
-        'd_ff':32,
-        'num_heads':3,
-        'd_kv':8,
-        'dropout_rate':0.1,
+        'd_model':256, # encoder 隐藏层状态
+        'd_ff':512,
+        'num_heads':8,
+        'd_kv':64,
+        'dropout_rate':0.3,
         'feed_forward_proj':'relu',
-        'input_emb_dim':32, # Bert embedding dimension
-        'target_emb_dim':32,
+        'input_emb_dim':768, # Bert embedding dimension
+        'target_emb_dim':768,
         'temperature':0.07, # infoNCE temperature
         'log_path':'./logs/tiger.log',
         'seed':42,
         'save_path':'./ckpt/tiger.pth',
         'early_stop':10,
         'topk_list':[5,10,20],
+        'loss_plot_path':'./loss_picture/task1.png'
     }
 def main():
     train(params)
